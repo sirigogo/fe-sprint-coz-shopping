7f7909a6b15d038b2fd081988333c96f7700c451
gsjang94@gamil.com <gsjang94@gmail.com>
2023-05-16 14:24:22 +0900

    Fixes bug

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
 import App from "./App";
-
-function reducer(state) {
-  return state;
-}
-
-let store = createStore(reducer);
+import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("CozShopping"));
 root.render(
